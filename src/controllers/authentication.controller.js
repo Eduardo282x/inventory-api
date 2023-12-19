e5dbae945d1233a17bc104697ece6484534a62d3
Eduardo <eduardokunmixyeah@gmail.com>
2023-12-18 21:09:34 -0400

    clean code

--- src/controllers/authentication.controller.js
+++ src/controllers/authentication.controller.js
@@ -1,7 +1,4 @@
 import { getConnection }  from '../database/database'
-// import { Jwt } from 'jsonwebtoken';
-// const jwt = require('jsonwebtoken');
-// const secretKey = 'my_secret_key';
 
 const authenticateUser = async (req, res) =>{
     try {
@@ -9,20 +6,13 @@ const authenticateUser = async (req, res) =>{
         const User = { Username, Password}
         const connection = await getConnection();
         const result = await connection.query(`SELECT * FROM users WHERE Username='${Username}' and Password='${Password}'`);
-        // console.log(result);
         if(result.length > 0){
-            // const parseJsonString = JSON.stringify(result[0])
             try{
-                // const plainObject = JSON.parse(parseJsonString);
-                // const token = jwt.sign(plainObject, secretKey, {
-                //     expiresIn: '2m'
-                // });
                 res.json({message:'Bienvenido', success: true, userData: result[0]});
             }
             catch(ex){
                 console.log(ex);
             }
-
         } else {
             res.json({message:'Usuario no encontrado', success: false});
         }
@@ -34,5 +24,5 @@ const authenticateUser = async (req, res) =>{
 }
 
 export const methods = {
-    authenticateUser
+    authenticateUser,
 };
